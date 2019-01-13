a95c905982cda5bd5a0353cc36767d27a03a6f6c
fm@postminister.com <fm@postminister.com>
2019-01-13 17:58:26 +0100

    finished section 16

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -37,3 +37,43 @@ Route::get('/read', function () {
     }
 
 });
+
+Route::get('/update', function () {
+
+    $staff = Staff::findOrFail(1);
+
+    $photo = $staff->photos()->whereId(1)->first();
+
+    $photo->path = 'update.png';
+
+    $photo->save();
+
+});
+
+Route::get('/delete', function () {
+
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->whereId(1)->delete();
+
+});
+
+Route::get('/assign', function () {
+
+    $staff = Staff::findOrFail(1);
+
+    $photo = Photo::findOrFail(3);
+
+    $staff->photos()->save($photo);
+
+});
+
+Route::get('/unassign', function () {
+
+    $staff = Staff::findOrFail(1);
+
+    $photo = Photo::findOrFail(3);
+
+    $staff->photos()->whereId(3)->update(['imageable_id' => '', 'imageable_type' => '']);
+
+});
